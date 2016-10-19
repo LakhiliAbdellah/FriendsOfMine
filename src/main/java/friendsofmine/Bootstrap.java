b253023a634e3bfa601e20dd1009de0cd7687dd4
Franck Silvestre <franck.silvestre@ticetime.com>
2016-10-19 12:00:27 +0200

    Mise en place de la classe bootstrap pour l'initialisation de l'application et des tests au lancement

---
+++ src/main/java/friendsofmine/Bootstrap.java
@@ -0,0 +1,89 @@
+package friendsofmine;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Bootstrap of the application
+ */
+@Component
+public class Bootstrap {
+
+    @Autowired private ActiviteService activiteService;
+
+    private Utilisateur mary;
+    private Utilisateur thom;
+    private Activite randonnee;
+    private Activite lindyHop;
+    private Activite taekwondo;
+
+
+    @PostConstruct
+    public void init() {
+        initMary();
+        initAndSaveRandonnee();
+        initAndSaveLindyHop();
+        initThom();
+        initAndSaveTaekwondo();
+    }
+
+    public Utilisateur getMary() {
+        return mary;
+    }
+
+    public Utilisateur getThom() {
+        return thom;
+    }
+
+    public Activite getRandonnee() {
+        return randonnee;
+    }
+
+    public Activite getLindyHop() {
+        return lindyHop;
+    }
+
+    public Activite getTaekwondo() {
+        return taekwondo;
+    }
+
+    private void initAndSaveTaekwondo() {
+        taekwondo = new Activite();
+        taekwondo.setTitre("Taekwondo");
+        taekwondo.setResponsable(thom);
+        activiteService.saveActivite(taekwondo);
+    }
+
+    private void initThom() {
+        thom = new Utilisateur();
+        thom.setNom("Thom");
+        thom.setPrenom("Thom");
+        thom.setEmail("thom@thom.com");
+        thom.setSexe("M");
+    }
+
+    private void initAndSaveLindyHop() {
+        lindyHop = new Activite();
+        lindyHop.setTitre("Lindy Hop");
+        lindyHop.setResponsable(mary);
+        activiteService.saveActivite(lindyHop);
+    }
+
+    private void initAndSaveRandonnee() {
+        randonnee = new Activite();
+        randonnee.setTitre("Randonnee");
+        randonnee.setResponsable(mary);
+        activiteService.saveActivite(randonnee);
+    }
+
+    private void initMary() {
+        mary = new Utilisateur();
+        mary.setNom("Mary");
+        mary.setPrenom("Mary");
+        mary.setEmail("mary@mary.com");
+        mary.setSexe("F");
+    }
+
+}
