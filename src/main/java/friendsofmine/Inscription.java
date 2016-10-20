97ea9801b02319d83dee43bab43deab4e750149e
Franck Silvestre <franck.silvestre@ticetime.com>
2016-10-20 17:27:07 +0200

    Partie 3, exercice 1 (classe Inscription) OK

---
+++ src/main/java/friendsofmine/Inscription.java
@@ -0,0 +1,55 @@
+package friendsofmine;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/**
+ * Class representing an Inscription
+ */
+@Entity
+public class Inscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @ManyToOne
+    private Utilisateur utilisateur;
+
+    @NotNull
+    @ManyToOne
+    private Activite activite;
+
+    @NotNull
+    private Date dateInscription;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Utilisateur getUtilisateur() {
+        return utilisateur;
+    }
+
+    public void setUtilisateur(Utilisateur utilisateur) {
+        this.utilisateur = utilisateur;
+    }
+
+    public Activite getActivite() {
+        return activite;
+    }
+
+    public void setActivite(Activite activite) {
+        this.activite = activite;
+    }
+
+    public Date getDateInscription() {
+        return dateInscription;
+    }
+
+    public void setDateInscription(Date dateInscription) {
+        this.dateInscription = dateInscription;
+    }
+}
